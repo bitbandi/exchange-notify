5626b19c9feda7c41a6ade931f280644ebe20668
elbandi <bandispam@gmail.com>
2026-06-17 00:49:14 +0200

    Swap kraken eur/btc symbol

--- app/Helpers/CcxtKraken.php
+++ app/Helpers/CcxtKraken.php
@@ -9,6 +9,12 @@ class CcxtKraken extends BaseKraken
     public function fetch_my_trades(?string $symbol = null, ?int $since = null, ?int $limit = null, $params = array ())
     {
         $orders =  parent::fetch_closed_orders($symbol, $since, $limit, $params);
+        foreach($orders as &$o) {
+            // swap currency symbols
+            if ($o["symbol"] == "BTC/EUR") {
+                $o["symbol"] = "EUR/BTC";
+            }
+        }
         return $orders;
     }
 }
